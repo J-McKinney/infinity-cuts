f5d28e7bf25c3bdb042fcc9f03ec450395a4c740
Jesse <jessemckinney513@gmail.com>
2021-02-12 00:49:48 -0500

    yay

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Style from "./Footer.module.css";
 
 function Footer(props) {
   return (
     <>
       <div className={Style.wrapper}>
-        <h1 className={Style.h1}>Footer</h1>
-        <div className={Style.topContainer}>
-          <div className={Style.topRow}>
-            <div className={Style.leftCol}></div>
-            <div className={Style.rightCol}></div>
-          </div>
-        </div>
+        <Jumbotron>
+          <Row>
+            <Col className="col-sm-6">Hello</Col>
+            <Col className="col-sm-6">World</Col>
+          </Row>
+        </Jumbotron>
       </div>
     </>
   );
