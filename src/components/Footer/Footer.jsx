ba79dc422eebdf5aaa2b94d24e727aa34c4fa60a
Jesse <jessemckinney513@gmail.com>
2021-02-16 11:43:28 -0500

    yay

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,20 +15,78 @@ function Footer(props) {
               <Col id={Style.ColOne} className="col-md-7">
                 <Row id="leftRow" className={Style.leftRow}>
                   <Col id="firstLeftCol" className={Style.firstLeftCol}>
-                    <h3 id="h3" className={Style.content}>
-                      Hello
-                    </h3>
+                    <ul>
+                      <h3 id="h3" className={Style.firstColContent}>
+                        Shop
+                      </h3>
+                      <li>
+                        <a
+                          href="/search"
+                          title="Search"
+                          className={Style.searchLink}
+                        >
+                          <span>Search</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="/pages/ContactUsPage"
+                          title="Contact Us"
+                          className={Style.contactLink}
+                        >
+                          <span>Contact Us</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="/pages/WishListPage"
+                          title="Wish List"
+                          className={Style.wishListLink}
+                        >
+                          <span>Wish List</span>
+                        </a>
+                      </li>
+                    </ul>
                   </Col>
                   <Col id="secondLeftCol" className={Style.secondLeftCol}>
-                    <h3 id="h3" className={Style.content}>
-                      World
-                    </h3>
+                    <ul>
+                      <h3 id="h3" className={Style.secondColContent}>
+                        Information
+                      </h3>
+                      <li>
+                        <a
+                          href="/pages/PrivacyPolicyPage"
+                          title="Privacy Policy"
+                          className={Style.privacyPolicyLink}
+                        >
+                          <span>Privacy Policy</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="/pages/RefundPolicyPage"
+                          title="Refund Policy"
+                          className={Style.refundPolicyLink}
+                        >
+                          <span>Refund Policy</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="/pages/TermsOfServicePage"
+                          title="Terms Of Service"
+                          className={Style.termsOfServiceLink}
+                        >
+                          <span>Terms Of Service</span>
+                        </a>
+                      </li>
+                    </ul>
                   </Col>
                 </Row>
               </Col>
               <Col id={Style.ColTwo} className="col-md-5">
-                <h3 id="h3" className={Style.content}>
-                  HelloWorld
+                <h3 id="h3" className={Style.thirdColContent}>
+                  Stay Connected
                 </h3>
               </Col>
             </Row>
